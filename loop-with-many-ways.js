5a3d75d3d21e2e75b09d3b5d4fe9d12194bb3146
mohamedisakr <mohamedisakr@gmail.com>
2021-06-16 09:04:28 +0200

    implementing loop with 6 ways

--- loop-with-many-ways.js
+++ loop-with-many-ways.js
@@ -1,18 +1,87 @@
-const axios = require("axios");
+// const axios = require("axios");
+const data = require("./airports-data");
 // exercise
 // loop through the following API with 6 ways
 
+// 1. using forEach
+data.forEach((item) => {
+  const { name, city, country } = item.attributes;
+  console.log({ name, city, country });
+});
+
+// 2. for
+for (let i = 0; i < data.length; i++) {
+  // console.log(data[i]);
+  const { name, city, country } = data[i].attributes;
+  console.log({ name, city, country });
+}
+
+// 3. for...of
+for (const item of data) {
+  const { name, city, country } = item.attributes;
+  console.log({ name, city, country });
+}
+
+// 4. while
+let index = 0;
+while (index < data.length) {
+  const { name, city, country } = data[index].attributes;
+  console.log({ name, city, country });
+  index++;
+}
+
+// 5. do...while
+let index = 0;
+do {
+  const { name, city, country } = data[index].attributes;
+  console.log({ name, city, country });
+  index++;
+} while (index < data.length);
+
+// 6. map
+data.map((item) => {
+  const { name, city, country } = item.attributes;
+  console.log({ name, city, country });
+});
+
+/*
 const url = `https://airportgap.dev-tester.com/api/airports`;
 let airports = [];
 
 const getAirports = async () => {
-  const res = await axios.get(url);
-  airports = [...res.data.data];
-  //   console.log(airports);
+  try {
+    const res = await axios.get(url);
+    airports = await [...res.data.data];
+  } catch (err) {
+    console.error(err);
+  }
+  // console.log(airports);
   return airports;
 };
 
-// airportList = getAirports();
+// let airports = getAirports()
+//   .then((result) => {
+//     return result;
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+// console.log(airports);
+
+// let airportList = getAirports()
+//   .then((result) => {
+//     airports = [...result];
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 // console.log(airportList);
-getAirports();
-console.log(airports);
+
+let airportList = (async () => {
+  const response = await getAirports();
+  // console.log(response);
+  airports = [...response];
+})();
+console.log(airportList);
+// console.log(airports);
+*/
