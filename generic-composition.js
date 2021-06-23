2cbdec096086925504544c6f452442a1f47fbb28
mohamedisakr <mohamedisakr@gmail.com>
2021-06-23 14:23:46 +0200

    implementing generic composition function

---
+++ generic-composition.js
@@ -0,0 +1,12 @@
+// function composition using pipe of any number of functions
+const pipe =
+  (...fns) =>
+  (x) =>
+    fns.reduce((y, f) => f(y), x);
+
+const double = (x) => x * 2;
+const square = (x) => x * x;
+
+// function pipe
+var output_final = pipe(square, double)(2);
+console.log(output_final);
